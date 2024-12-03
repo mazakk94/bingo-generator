d641615dca1af1b026e098942144a198e0213bca
Maciej Wu <wojtasik@pm.me>
2024-12-03 12:56:45 +0100

    initial commit

---
+++ src/main/java/com/mrq/bingo/BingoGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.mrq.bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BingoGeneratorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mrq/bingo/BingoGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrq.bingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingoGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
